5c0ceaa899418c7d6af97fd5572ca5c4734285a4
serendipity <sju0924@khu.ac.kr>
2025-03-20 15:31:16 +0900

    Fix directory architecture of send server lambda

--- amplify/functions/autoSendServer/resource.ts
+++ amplify/functions/autoSendServer/resource.ts
@@ -4,5 +4,5 @@ export const autoSendServer = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'autoSendServer',
   // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './handler.ts'
+  entry: './src/handler.ts'
 });
