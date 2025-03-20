9d10716deb5fa86038eec1f0339449b2d33deaf8
serendipity <sju0924@khu.ac.kr>
2025-03-20 14:50:57 +0900

    Remove env console

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ import store from './store'
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
-// Kakao SDK 초기화 (index.html에서 로드된 스크립트를 활용)
-console.log(import.meta.env.VITE_KAKAO_APP_KEY)
-
 
 createApp(App)
   .use(router)
