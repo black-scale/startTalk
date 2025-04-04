b6906bfc73e2c2dacc426d6bde2905814f3fa6c9
serendipity <sju0924@khu.ac.kr>
2025-04-05 00:51:58 +0900

    update amplify.yml

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import router from './router'
 import store from './store'
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
