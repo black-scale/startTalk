10e166aabc2e996098dd2c0e95b8fa607c2355d0
serendipity <sju0924@khu.ac.kr>
2025-05-05 18:32:31 +0900

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -38,7 +38,7 @@ const schema = a.schema({
       room: a.string(),   
       
       createdAt: a.string(),    // 세션 생성 시각
-      expireAt: a.string(),
+      expireAt: a.integer(),
       updatedAt: a.datetime()
       
     })
